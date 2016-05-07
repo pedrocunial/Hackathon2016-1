e7ee2b74f08498598c2ae7f73f6dd3e9cbbc4cab
Pedro Cunial <pedrocc4@al.insper.edu.br>
2016-05-07 15:24:39 -0300

    bla

--- app/src/main/java/br/edu/insper/hackathon2/HeartBeatActivity.java
+++ app/src/main/java/br/edu/insper/hackathon2/HeartBeatActivity.java
@@ -1,45 +1,65 @@
 package br.edu.insper.hackathon2;
 
+import android.annotation.TargetApi;
+import android.os.Build;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Objects;
 import java.util.Random;
 
 public class HeartBeatActivity extends AppCompatActivity {
 
-    Random r;
+    private int heartbeat;
+    private Random r;
+    private final int high = 190;
+    private final int low = 100;
+    private EditText mensagem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_heart_beat);
 
-        int high = 190;
-        int low = 100;
 
         r = new Random();
-        int heartbeat;
 
+        heartbeat = r.nextInt(high - low) + low;
 
+        this.mensagem = (EditText) findViewById(R.id.text_heart_beat);
 
-        while(true) {
+        assert this.mensagem != null;
+        this.mensagem.setText(String heartbeat);
 
-            heartbeat = r.nextInt(high - low) + low;
-
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-
-            if(heartbeat > 170) {
-                Toast.makeText(HeartBeatActivity.this, "Morreu!", Toast.LENGTH_SHORT).show();
-
+        final CountDownTimer espaco = new CountDownTimer(1000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
 
             }
 
-        }
+            @TargetApi(Build.VERSION_CODES.KITKAT)
+            @Override
+            public void onFinish() {
+                heartbeat = r.nextInt(high - low) + low;
+                System.out.println(heartbeat);
+                mensagem.setText(heartbeat);
+
+                if(heartbeat > 170) {
+                    Toast.makeText(HeartBeatActivity.this, "Morreu!", Toast.LENGTH_SHORT).show();
+
+                }
+            }
+
+        }.start();
+
+        espaco.cancel();
+        espaco.start();
+
+
 
 
 
