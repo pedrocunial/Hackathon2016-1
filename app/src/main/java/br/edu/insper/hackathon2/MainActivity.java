fd0102fa81aa8e94859523ebfbf8f41710d1e3d3
Pedro Cunial <pedrocc4@al.insper.edu.br>
2016-05-07 13:53:46 -0300

    criando botoes

--- app/src/main/java/br/edu/insper/hackathon2/MainActivity.java
+++ app/src/main/java/br/edu/insper/hackathon2/MainActivity.java
@@ -2,6 +2,7 @@ package br.edu.insper.hackathon2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttonHeart = (Button) findViewById(R.id.btn_heartbeat);
+
 
 
     }
