58ba90792980b011c1b012921249352a852e0d3e
Pedro Cunial <pedrocc4@al.insper.edu.br>
2016-05-07 14:22:12 -0300

    bla

--- app/src/main/java/br/edu/insper/hackathon2/MainActivity.java
+++ app/src/main/java/br/edu/insper/hackathon2/MainActivity.java
@@ -3,6 +3,7 @@ package br.edu.insper.hackathon2;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,14 +12,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        TextView textView = (TextView) findViewById(R.id.text_title);
+
         Button buttonHeart = (Button) findViewById(R.id.btn_heartbeat);
         Button buttonTemp = (Button) findViewById(R.id.btn_temp);
         Button buttonMove = (Button) findViewById(R.id.btn_move);
         Button buttonOxigen = (Button) findViewById(R.id.btn_oxigen);
-
-
-
-
 
 
     }
