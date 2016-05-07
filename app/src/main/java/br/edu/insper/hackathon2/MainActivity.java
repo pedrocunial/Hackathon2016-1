26eef8820308c10900ed5c3819b39ef0fa65ddbc
gabicfa <gabrielacfa@al.insper.edu.br>
2016-05-07 16:12:49 -0300

    bla

--- app/src/main/java/br/edu/insper/hackathon2/MainActivity.java
+++ app/src/main/java/br/edu/insper/hackathon2/MainActivity.java
@@ -1,7 +1,9 @@
 package br.edu.insper.hackathon2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -19,6 +21,15 @@ public class MainActivity extends AppCompatActivity {
         Button buttonMove = (Button) findViewById(R.id.btn_move);
         Button buttonOxigen = (Button) findViewById(R.id.btn_oxigen);
 
+        assert buttonTemp != null;
+        buttonTemp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                startActivity(new Intent(MainActivity.this, TempActivity.class));
+
+            }
+        });
 
     }
 }
